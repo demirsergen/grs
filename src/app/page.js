c25e6cd3b51989b0154c471ca7fbaf9136a3f9eb
Sergen Demir <dmrsrgn@gmail.com>
2024-03-25 14:39:45 -0500

    update styling

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-teal-800">
+    <main className="flex min-h-screen flex-col items-center p-24 bg-teal-800">
       <h1 className="text-white">GRS</h1>
       <MultiStepForm />
     </main>
